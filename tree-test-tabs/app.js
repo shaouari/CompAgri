a4aaf7b9ef047d795ff5e0f0f0a40346c5d87efd
enjoythelive1 <enjoythelive1@gmail.com>
2015-02-08 13:30:38 -0400

    new node adding bug fixed

--- tree-test-tabs/app.js
+++ tree-test-tabs/app.js
@@ -162,10 +162,10 @@
 
                 $scope.newSubItem = function (scope) {
                     var nodeData = scope.$modelValue;
-                    nodeData.nodes.push({
-                        id: nodeData.id * 10 + nodeData.nodes.length + 1,
-                        title: nodeData.title + '.' + (nodeData.nodes.length + 1),
-                        nodes: []
+                    nodeData.items.push({
+                        id: nodeData.id * 10 + nodeData.items.length + 1,
+                        title: nodeData.title + '.' + (nodeData.items.length + 1),
+                        items: []
                     });
                     $scope.redrawLines();
                 };
